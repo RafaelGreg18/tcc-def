7551b8f075c803fb171930eff670fd00556d815d
filipemr <filipe.mr@ufc.br>
2025-08-29 22:24:18 -0300

    Experiment: script motivational experiment 1 - performance x cost without battery.

--- run_motiv_experiment_1.sh
+++ run_motiv_experiment_1.sh
@@ -10,15 +10,22 @@ export CUDA_VISIBLE_DEVICES=$1
 
 # Run each configuration 3 times
 for i in {1..3}; do
+  echo "Seed $i"
   # criar modelo
+  echo "Criando modelo"
   python gen_sim_model.py --seed $i
 
   # criar perfis
+  echo "Criando perfis"
   python gen_sim_cid_profile.py --seed $i
 
   #participantsxperformancexcost
   for participants in 5 10 20 40 80 100; do
+    echo "Participants $participants"
+
     for alpha in 0.1 0.3 1.0; do
+      echo "Alpha $alpha"
+
       flwr run . gpu-sim-dl --run-config="seed=$i num-participants=$participants dir-alpha=$alpha"
     done
   done
