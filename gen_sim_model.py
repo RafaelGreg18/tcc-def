fd1dd784a3f9363403eaf3033784e7e5413bd143
filipemr <filipe.mr@ufc.br>
2025-08-31 09:10:48 -0300

    Experiment-1:new config for mobilenet.

--- gen_sim_model.py
+++ gen_sim_model.py
@@ -10,9 +10,9 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--config_file", type=str, default="./pyproject.toml")
     parser.add_argument("--seed", type=int, default=42)
-    parser.add_argument("--model_name", type=str, default="simplecnn")
-    parser.add_argument("--input_shape", type=str, default="(3,32,32)")
-    parser.add_argument("--num_classes", type=int, default=10)
+    # parser.add_argument("--model_name", type=str, default="simplecnn")
+    # parser.add_argument("--input_shape", type=str, default="(3,32,32)")
+    #     parser.add_argument("--num_classes", type=int, default=10)
     args = parser.parse_args()
 
     # Read config simulation file and validate it
@@ -26,9 +26,9 @@ def main():
 
     # Get valid config
     cfg = config_repo.get_app_config()
-    model_name = args.model_name
-    input_shape = literal_eval(args.input_shape)
-    num_classes = args.num_classes
+    model_name = cfg["model-name"]
+    input_shape = cfg["input-shape"]
+    num_classes = cfg["num-classes"]
 
     # Creating a model
     model = ModelFactory.create(model_name=model_name, input_shape=input_shape, num_classes=num_classes)
