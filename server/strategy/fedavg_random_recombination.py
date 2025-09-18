52191dbaa8c9bc27f04076cf885c7df572384a88
filipemr <filipe.mr@ufc.br>
2025-09-18 11:30:52 -0300

    Test: expanding tests of recombination strategy to see if it is a true alternative.

---
+++ server/strategy/fedavg_random_recombination.py
@@ -0,0 +1,161 @@
+import ast
+import copy
+import datetime
+import math
+import os
+import random
+from logging import WARNING
+from typing import Optional
+
+import torch
+from flwr.common import FitIns, Parameters, Scalar, parameters_to_ndarrays, ndarrays_to_parameters, log
+from flwr.server.client_proxy import ClientProxy
+from flwr.server.strategy.aggregate import aggregate, weighted_loss_avg
+from requests import delete
+
+from server.strategy.base import BaseStrategy
+from utils.model.manipulation import set_weights, get_weights, ModelPersistence
+
+
+class FedAvgRandomRecombination(BaseStrategy):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def _do_initialization(self, client_manager):
+        current_date = datetime.datetime.now().strftime("%d-%m-%Y")
+        selection_name = self.context.run_config["selection-name"]
+        aggregation_name = self.context.run_config["aggregation-name"]
+        participants_name = self.context.run_config["participants-name"]
+        dataset_id = self.context.run_config["hugginface-id"].split('/')[-1]
+        seed = self.context.run_config["seed"]
+        dir = self.context.run_config["dir-alpha"]
+
+        output_dir = os.path.join("outputs", current_date,
+                                  f"{aggregation_name}_{selection_name}_{participants_name}_{self.num_participants}_battery_{self.use_battery}_dataset_{dataset_id}_dir_{dir}_seed_{seed}")
+        os.makedirs(output_dir, exist_ok=True)
+
+        self.model_performance_path = os.path.join(output_dir, "model_performance.json")
+        self.system_performance_path = os.path.join(output_dir, "system_performance.json")
+        self.fl_cli_state_path = os.path.join(output_dir, "client_state.json")
+
+    def num_fit_clients(self, num_available_clients: int) -> tuple[int, int]:
+        return self.num_participants, self.num_participants
+
+    def num_evaluation_clients(self, num_available_clients: int) -> tuple[int, int]:
+        return self.num_evaluators, self.num_participants # num_eval > num_part
+
+    def _do_configure_fit(self, server_round, parameters, client_manager) -> list[tuple[ClientProxy, FitIns]]:
+        config = {}
+        if self.on_fit_config_fn is not None:
+            # Custom fit config function provided
+            config = self.on_fit_config_fn(server_round)
+        fit_ins = FitIns(parameters, config)
+
+        # Sample clients
+        sample_size, min_num_clients = self.num_fit_clients(
+            client_manager.num_available()
+        )
+        clients = client_manager.sample(
+            num_clients=sample_size, min_num_clients=min_num_clients
+        )
+
+        # Return client/config pairs
+        return [(client, fit_ins) for client in clients]
+
+    def _do_aggregate_fit(self, server_round, results, failures) -> tuple[Optional[Parameters], dict[str, Scalar]]:
+        """Aggregate fit results using weighted average."""
+        if not results:
+            return None, {}
+        # Do not aggregate if there are failures and failures are not accepted
+        if failures:
+            return None, {}
+
+        # Convert results
+        weights_results = [
+            (parameters_to_ndarrays(fit_res.parameters), fit_res.num_examples)
+            for _, fit_res in results
+        ]
+
+        # Virtual clients
+        min_num_examples = math.inf
+        for result in weights_results:
+            if result[1] < min_num_examples:
+                min_num_examples = result[1]
+
+
+        recombined_models = []
+        model_name = self.context.run_config['model-name']
+        input_shape = self.context.run_config['input-shape']
+        num_classes = self.context.run_config['num-classes']
+        root_model_dir = self.context.run_config["root-model-dir"]
+        model_path = root_model_dir + model_name + '.pth'
+        model = ModelPersistence.load(model_path, model_name, input_shape=input_shape, num_classes=num_classes)
+
+        for _, fit_res in results:
+            ndarrays = parameters_to_ndarrays(fit_res.parameters)
+            set_weights(model, ndarrays)
+            recombined_models.append(copy.deepcopy(model))
+
+        recombined_models = self.recombination(recombined_models)
+
+        for model in recombined_models:
+            ndarrays = get_weights(model)
+            weights_results.append((ndarrays, min_num_examples))
+
+        aggregated_ndarrays = aggregate(weights_results)
+        parameters_aggregated = ndarrays_to_parameters(aggregated_ndarrays)
+
+        # Aggregate custom metrics if aggregation fn was provided
+        metrics_aggregated = {}
+        if self.fit_metrics_aggregation_fn:
+            fit_metrics = [(res.num_examples, res.metrics) for _, res in results]
+            metrics_aggregated = self.fit_metrics_aggregation_fn(fit_metrics)
+        elif server_round == 1:  # Only log this warning once
+            log(WARNING, "No fit_metrics_aggregation_fn provided")
+
+        return parameters_aggregated, metrics_aggregated
+
+    def _do_aggregate_evaluate(self, server_round, results, failures) -> tuple[Optional[float], dict[str, Scalar]]:
+        if not results:
+            return None, {}
+        # Do not aggregate if there are failures and failures are not accepted
+        if failures:
+            return None, {}
+
+        # Aggregate loss
+        loss_aggregated = weighted_loss_avg(
+            [
+                (evaluate_res.num_examples, evaluate_res.loss)
+                for _, evaluate_res in results
+            ]
+        )
+
+        # Aggregate custom metrics if aggregation fn was provided
+        metrics_aggregated = {}
+        if self.evaluate_metrics_aggregation_fn:
+            eval_metrics = [(res.num_examples, res.metrics) for _, res in results]
+            metrics_aggregated = self.evaluate_metrics_aggregation_fn(eval_metrics)
+        elif server_round == 1:  # Only log this warning once
+            log(WARNING, "No evaluate_metrics_aggregation_fn provided")
+
+        return loss_aggregated, metrics_aggregated
+
+    def recombination(self, models):
+        # models: lista de nn.Module (mesma arquitetura)
+
+        with torch.no_grad():
+            sds = [m.state_dict() for m in models]
+            new_sds = [copy.deepcopy(sd) for sd in sds]
+
+            nr = list(range(self.num_participants))
+            keys = list(sds[0].keys())
+            for k in keys:
+                random.shuffle(nr)
+                for i in range(self.num_participants):
+                    new_sds[i][k].copy_(sds[nr[i]][k])  # copia in-place
+
+            new_models = [copy.deepcopy(m) for m in models]
+            for m, sd in zip(new_models, new_sds):
+                m.load_state_dict(sd)
+
+        return new_models
--- utils/simulation/workflow.py
+++ utils/simulation/workflow.py
@@ -10,6 +10,7 @@ from torch.utils.data import DataLoader
 from server.strategy.fedavg_random_constant import FedAvgRandomConstant
 from server.strategy.fedavg_random_constant_twophase import FedAvgRandomConstantTwoPhase
 from server.strategy.fedavg_random_criticalfl import FedAvgRandomCPEval
+from server.strategy.fedavg_random_recombination import FedAvgRandomRecombination
 from utils.dataset.partition import DatasetFactory
 from utils.model.manipulation import ModelPersistence, get_weights, set_weights, test
 from utils.simulation.config import ConfigRepository
@@ -155,7 +156,8 @@ def get_fit_metrics_aggregation_fn(is_critical: bool):
             # fgn
             gns = [m["gn"] for _, m in metrics]
             # Aggregate and return custom metric (weighted average)
-            return {"acc": sum(accuracies) / sum(examples), "loss": sum(losses) / sum(examples), "avg_gn": sum(gns)/len(gns)}
+            return {"acc": sum(accuracies) / sum(examples), "loss": sum(losses) / sum(examples),
+                    "avg_gn": sum(gns) / len(gns)}
         else:
             return {"acc": sum(accuracies) / sum(examples), "loss": sum(losses) / sum(examples)}
 
@@ -227,6 +229,19 @@ def get_strategy(context: Context, initial_parameters: Parameters, fit_metrics_a
                                               on_fit_config_fn=on_fit_config_fn,
                                               on_eval_config_fn=on_eval_config_fn,
                                               evaluate_fn=evaluate_fn)
+            elif participants_name == "recombination":
+                strategy = FedAvgRandomRecombination(repr="FedAvgRandomRecombination",
+                                                     num_clients=num_clients,
+                                                     profiles=profiles,
+                                                     num_participants=num_participants,
+                                                     num_evaluators=num_evaluators,
+                                                     context=context,
+                                                     initial_parameters=initial_parameters,
+                                                     fit_metrics_aggregation_fn=fit_metrics_aggregation_fn,
+                                                     evaluate_metrics_aggregation_fn=evaluate_metrics_aggregation_fn,
+                                                     on_fit_config_fn=on_fit_config_fn,
+                                                     on_eval_config_fn=on_eval_config_fn,
+                                                     evaluate_fn=evaluate_fn)
 
     return strategy
 
