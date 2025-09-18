b9b52729c0487ed2fb15836b5f6c34b963b50d98
filipemr <filipe.mr@ufc.br>
2025-09-18 11:13:31 -0300

    Motivational: swapping experiments id's 2<->3.

--- run_motiv_experiment_3.sh
+++ run_motiv_experiment_3.sh
@@ -9,18 +9,38 @@ fi
 export CUDA_VISIBLE_DEVICES=$1
 
 # Run each configuration 3 times
-for i in {1..3}; do
-  echo "Seed $i"
+#for i in {1..3}; do
+#  echo "Seed $i"
   # criar modelo
-  echo "Criando modelo"
-  python gen_sim_model.py --seed $i
+#  echo "Criando modelo"
+#  python gen_sim_model.py --seed $i
 
   # criar perfis
-  echo "Criando perfis"
-  python gen_sim_profile.py --seed $i
+#  echo "Criando perfis"
+#  python gen_sim_profile.py --seed $i
 
-  for alpha in 0.1 0.3 1.0; do
-    echo "Alpha $alpha"
-    flwr run . gpu-sim-dl-24 --run-config="seed=$i  num-rounds=300 is-critical=true participants-name='criticalfl' dir-alpha=$alpha"
-  done
-done
+  #participantsxperformancexcost
+#  for participants_bcp in 5 10 20; do
+#    echo "Participants before critical point $participants_bcp"
+#    for participants_acp in 5 10 20; do
+#      echo "Participants after critical point $participants_acp"
+#      for alpha in 0.1 0.3 1.0; do
+#        echo "Alpha $alpha"
+#        flwr run . gpu-sim-lrc --run-config="seed=$i participants-name='twophase' num-participants-bcp=$participants_bcp num-participants-acp=$participants_acp dir-alpha=$alpha"
+#      done
+#    done
+#  done
+#done
+
+# Manual config
+# seed 1
+# alpha 0.1
+python gen_sim_model.py --seed 1
+python gen_sim_profile.py --seed 1
+
+#fgn
+flwr run . gpu-sim-lrc --run-config="seed=1 participants-name='twophase' num-participants-bcp=10 num-participants-acp=5 dir-alpha=0.1 cp=25 num-rounds=50"
+flwr run . gpu-sim-lrc --run-config="seed=1 participants-name='twophase' num-participants-bcp=5 num-participants-acp=10 dir-alpha=0.1 cp=25 num-rounds=50"
+# derivative
+flwr run . gpu-sim-lrc --run-config="seed=1 participants-name='twophase' num-participants-bcp=10 num-participants-acp=5 dir-alpha=0.1 cp=125 num-rounds=250"
+flwr run . gpu-sim-lrc --run-config="seed=1 participants-name='twophase' num-participants-bcp=5 num-participants-acp=10 dir-alpha=0.1 cp=125 num-rounds=250"
