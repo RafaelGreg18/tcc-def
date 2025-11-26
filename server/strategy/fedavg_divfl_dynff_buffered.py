ada58dfeccc80e4f392d9717d97d97012c6c4c50
filipemr <filipe.mr@ufc.br>
2025-11-26 11:00:26 -0300

    Refactoring results folders + Fix dynffbuff into divfl and oort.

--- server/strategy/fedavg_divfl_dynff_buffered.py
+++ server/strategy/fedavg_divfl_dynff_buffered.py
@@ -2,12 +2,13 @@ import copy
 from logging import WARNING
 from typing import Optional
 
-from flwr.common import Parameters, Scalar, parameters_to_ndarrays, ndarrays_to_parameters, log
+from flwr.common import Parameters, Scalar, parameters_to_ndarrays, ndarrays_to_parameters, log, FitIns
+from flwr.server.client_proxy import ClientProxy
 from flwr.server.strategy.aggregate import aggregate
 
 from server.strategy.fedavg_divfl_dynff import FedAvgDivflDynff
 from utils.model.manipulation import ModelPersistence, set_weights
-from utils.strategy.divfl import get_gradients
+from utils.strategy.divfl import get_gradients, submod_sampling
 
 
 class FedAvgDivflDynffBuff(FedAvgDivflDynff):
@@ -18,6 +19,72 @@ class FedAvgDivflDynffBuff(FedAvgDivflDynff):
         self.parameters_to_agg = []
         self.last_params = None
         self.last_metrics = None
+        self.participants_stable = []
+        self.buffer_cids = []
+
+    def _do_configure_fit(self, server_round, parameters, client_manager) -> list[tuple[ClientProxy, FitIns]]:
+        config = {}
+        if self.on_fit_config_fn is not None:
+            # Custom fit config function provided
+            config = self.on_fit_config_fn(server_round)
+        fit_ins = FitIns(parameters, config)
+
+        # Se rodada é estável:
+        #   Se rodada é início do buffer:
+        #       Seleciona a quantidade total do buffer e amarzena cids em uma lista
+        #       Armazena lista de quantidade por rodada no buffer
+        #   Atualiza quantidade de participantes na rodada pegando da lista
+        # Se não é estável:
+        #   Se rodada é de verificação:
+        #       Verifica se alcançou estabilidade
+        #   Seleciona participantes da rodada
+        # Atualiza número de selecionados até o momento
+
+        if self.state["stable"]:
+            if server_round % 2 == 0:
+                self.participants_stable = []
+                base_idx = server_round - self.state["stable_round"]
+                start_participants = self.initial_num_participants + self.additions[base_idx]
+                end_participants = self.initial_num_participants + self.additions[base_idx+1]
+
+                self.participants_stable.append(start_participants)
+                self.participants_stable.append(end_participants)
+                self.num_participants = min(self.max_participants, start_participants)
+                self.num_selected.append(self.num_participants)
+
+                sample_size = min(start_participants + end_participants, client_manager.num_available())
+                self.buffer_cids = submod_sampling(self.gradients, sample_size, client_manager.num_available(),
+                                                stochastic=True)
+
+                selected_cids = self.buffer_cids[:self.num_participants]
+            else:
+                self.num_participants = min(self.max_participants, self.participants_stable[-1])
+                self.num_selected.append(self.num_participants)
+                selected_cids = self.buffer_cids[-self.num_participants:]
+        else:
+            if server_round % self.acks == 0 and server_round > 3 :
+                self.update_dynff(server_round)
+
+            sample_size, min_num_clients = self.num_fit_clients(
+                client_manager.num_available()
+            )
+
+            self.num_selected.append(sample_size)
+
+            selected_cids = submod_sampling(self.gradients, sample_size, client_manager.num_available(),
+                            stochastic=True)
+
+        selected_flwr_cids = []
+        for cid in selected_cids:
+            for key, value in self.cid_map.items():
+                if cid == value:
+                    selected_flwr_cids.append(key)
+                    break
+
+        clients = [client_manager.clients[flwr_cid] for flwr_cid in selected_flwr_cids]
+
+        # Return client/config pairs
+        return [(client, fit_ins) for client in clients]
 
     def _do_aggregate_fit(self, server_round, results, failures) -> tuple[Optional[Parameters], dict[str, Scalar]]:
         """Aggregate fit results using weighted average."""
@@ -43,7 +110,7 @@ class FedAvgDivflDynffBuff(FedAvgDivflDynff):
         self.gradients = get_gradients(self.global_model, self.local_models, self.num_clients)
 
         if self.state["stable"]:
-            if server_round % self.acks != 0:
+            if server_round % 2 != 0:
                 for _, fit_res in results:
                     self.parameters_to_agg.append((parameters_to_ndarrays(fit_res.parameters), fit_res.num_examples))
             else:
--- server/strategy/fedavg_oort_dynff_buffered.py
+++ server/strategy/fedavg_oort_dynff_buffered.py
@@ -1,7 +1,8 @@
 from logging import WARNING
 from typing import Optional
 
-from flwr.common import Parameters, Scalar, parameters_to_ndarrays, ndarrays_to_parameters, log
+from flwr.common import Parameters, Scalar, parameters_to_ndarrays, ndarrays_to_parameters, log, FitIns
+from flwr.server.client_proxy import ClientProxy
 from flwr.server.strategy.aggregate import aggregate
 
 from server.strategy.fedavg_oort_dynff import FedAvgOortDynff
@@ -15,6 +16,68 @@ class FedAvgOortDynffBuff(FedAvgOortDynff):
         self.parameters_to_agg = []
         self.last_params = None
         self.last_metrics = None
+        self.participants_stable = []
+        self.buffer_cids = []
+
+    def _do_configure_fit(self, server_round, parameters, client_manager) -> list[tuple[ClientProxy, FitIns]]:
+        config = {}
+        if self.on_fit_config_fn is not None:
+            # Custom fit config function provided
+            config = self.on_fit_config_fn(server_round)
+        fit_ins = FitIns(parameters, config)
+
+        # Cid utility map
+        cids_utility = []
+        for cid in self.available_cids:
+            cids_utility.append((self.profiles[cid]['utility'], cid))
+
+        if self.state["stable"]:
+            if server_round % 2 == 0:
+                self.participants_stable = []
+                base_idx = server_round - self.state["stable_round"]
+                start_participants = self.initial_num_participants + self.additions[base_idx]
+                end_participants = self.initial_num_participants + self.additions[base_idx+1]
+
+                self.participants_stable.append(start_participants)
+                self.participants_stable.append(end_participants)
+                self.num_participants = min(self.max_participants, start_participants)
+                self.num_selected.append(self.num_participants)
+
+                sample_size = min(start_participants + end_participants, client_manager.num_available())
+                self.buffer_cids = self.sample_fit(client_manager, sample_size, cids_utility)
+
+                selected_cids = self.buffer_cids[:self.num_participants]
+                print(selected_cids)
+            else:
+                self.num_participants = min(self.max_participants, self.participants_stable[-1])
+                self.num_selected.append(self.num_participants)
+                selected_cids = self.buffer_cids[-self.num_participants:]
+                print(selected_cids)
+        else:
+            if server_round % self.acks == 0 and server_round > 3 :
+                self.update_dynff(server_round)
+
+            sample_size, min_num_clients = self.num_fit_clients(
+                client_manager.num_available()
+            )
+
+            self.num_selected.append(sample_size)
+
+            selected_cids = self.sample_fit(client_manager, sample_size, cids_utility)
+
+        # Oort selection
+
+        selected_flwr_cids = []
+        for cid in selected_cids:
+            for key, value in self.cid_map.items():
+                if cid == value:
+                    selected_flwr_cids.append(key)
+                    break
+
+        clients = [client_manager.clients[flwr_cid] for flwr_cid in selected_flwr_cids]
+
+        # Return client/config pairs
+        return [(client, fit_ins) for client in clients]
 
     def _do_aggregate_fit(self, server_round, results, failures) -> tuple[Optional[Parameters], dict[str, Scalar]]:
         """Aggregate fit results using weighted average."""
@@ -26,12 +89,10 @@ class FedAvgOortDynffBuff(FedAvgOortDynff):
 
         # Convert results
         if self.state["stable"]:
-            if server_round % self.acks != 0:
-                for _, fit_res in results:
-                    self.parameters_to_agg.append((parameters_to_ndarrays(fit_res.parameters), fit_res.num_examples))
-            else:
-                for _, fit_res in results:
-                    self.parameters_to_agg.append((parameters_to_ndarrays(fit_res.parameters), fit_res.num_examples))
+            for _, fit_res in results:
+                self.parameters_to_agg.append((parameters_to_ndarrays(fit_res.parameters), fit_res.num_examples))
+
+            if server_round % 2 == 0:
                 aggregated_ndarrays = aggregate(self.parameters_to_agg)
                 parameters_aggregated = ndarrays_to_parameters(aggregated_ndarrays)
 
