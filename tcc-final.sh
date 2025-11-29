b07542b2c02daaa3ecae15ab89ab3e50cf1bbbe2
Rafael Gregori <rafaelgregori1337@gmail.com>
2025-11-29 12:53:39 -0300

    update final script

--- tcc-final.sh
+++ tcc-final.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
 
-# Check if an argument was provided
-# if [ -z "$1" ]; then
-#     echo "Usage: $0 <CUDA_DEVICE_IDS>"
-#     exit 1
-# fi
+#!/bin/bash
+#SBATCH --job-name=flower_galo_baseline
+#SBATCH --output=slurm.out
+#SBATCH --error=slurm.error
+#SBATCH --ntasks=1
+#SBATCH --time=96:00:00
+#SBATCH --mem=48G
+#SBATCH --cpus-per-task=32
+#SBATCH --gres=gpu:1
+#SBATCH --partition=l40s
+#SBATCH --mail-user=r247346@dac.unicamp.br 
+#SBATCH --mail-type=BEGIN,END,FAIL
 
-# export CUDA_VISIBLE_DEVICES=$1
+source ~/miniconda3/bin/activate
+conda activate flower
 
+export HF_HOME="~/galo/cache"
+export HF_HUB_OFFLINE=1
+
+echo "Criando modelo"
+python gen_sim_model.py --seed 42
+
+echo "Criando perfis"
+python gen_sim_profile.py --seed 42
+
+flwr run . --run-config="seed=42 num-rounds=150 selection-name='random' dir-alpha=0.1 use-battery=true"
+flwr run . --run-config="seed=42 num-rounds=150 selection-name='random' dir-alpha=1.0 use-battery=true"
 
 for eaflweight in 0.25 0.5 0.75; do
-
   for numedgeservers in 25 50 100; do
+    echo "Running EAFL+ with num-edge-servers=$numedgeservers and eafl-weight=$eaflweight"
     flwr run . --run-config="num-edge-servers=$numedgeservers seed=42 num-rounds=150 selection-name='eaflplus' dir-alpha=0.1 use-battery=true eafl-weight=$eaflweight"
     flwr run . --run-config="num-edge-servers=$numedgeservers seed=42 num-rounds=150 selection-name='eaflplus' dir-alpha=1.0 use-battery=true eafl-weight=$eaflweight"
   done
-
 done
