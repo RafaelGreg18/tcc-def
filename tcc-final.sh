69fe4ad293b066f49c992ba791bc285b93ff97a4
Rafael Gregori <rafaelgregori1337@gmail.com>
2025-11-29 12:55:11 -0300

    update final script

--- tcc-final.sh
+++ tcc-final.sh
@@ -25,13 +25,16 @@ python gen_sim_model.py --seed 42
 echo "Criando perfis"
 python gen_sim_profile.py --seed 42
 
+echo "Running Baseline with dir-alpha=0.1"
 flwr run . --run-config="seed=42 num-rounds=150 selection-name='random' dir-alpha=0.1 use-battery=true"
+echo "Running Baseline with dir-alpha=1.0"
 flwr run . --run-config="seed=42 num-rounds=150 selection-name='random' dir-alpha=1.0 use-battery=true"
 
 for eaflweight in 0.25 0.5 0.75; do
   for numedgeservers in 25 50 100; do
-    echo "Running EAFL+ with num-edge-servers=$numedgeservers and eafl-weight=$eaflweight"
+    echo "Running EAFL+ with num-edge-servers=$numedgeservers and eafl-weight=$eaflweight and dir-alpha=0.1"
     flwr run . --run-config="num-edge-servers=$numedgeservers seed=42 num-rounds=150 selection-name='eaflplus' dir-alpha=0.1 use-battery=true eafl-weight=$eaflweight"
+    echo "Running EAFL+ with num-edge-servers=$numedgeservers and eafl-weight=$eaflweight and dir-alpha=1.0"
     flwr run . --run-config="num-edge-servers=$numedgeservers seed=42 num-rounds=150 selection-name='eaflplus' dir-alpha=1.0 use-battery=true eafl-weight=$eaflweight"
   done
 done
