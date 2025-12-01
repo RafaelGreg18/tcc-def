833daba296b3b339ce399723d17070681dad8f1f
Rafael Gregori <rafaelgregori1337@gmail.com>
2025-11-30 22:51:24 -0300

    invert numedgeservers iteration

--- tcc-final.sh
+++ tcc-final.sh
@@ -31,7 +31,7 @@ echo "Running Baseline with dir-alpha=1.0"
 flwr run . --run-config="seed=42 num-rounds=150 selection-name='random' dir-alpha=1.0 use-battery=true"
 
 for eaflweight in 0.25 0.5 0.75; do
-  for numedgeservers in 25 50 100; do
+  for numedgeservers in 100 50 25; do
     echo "Running EAFL+ with num-edge-servers=$numedgeservers and eafl-weight=$eaflweight and dir-alpha=0.1"
     flwr run . --run-config="num-edge-servers=$numedgeservers seed=42 num-rounds=150 selection-name='eaflplus' dir-alpha=0.1 use-battery=true eafl-weight=$eaflweight"
     echo "Running EAFL+ with num-edge-servers=$numedgeservers and eafl-weight=$eaflweight and dir-alpha=1.0"
